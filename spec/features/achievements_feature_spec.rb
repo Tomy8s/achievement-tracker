1c3838fa5d0fb896053a3fd0f14ddbe6333d3233
George Eaton <george_eaton@hotmail.co.uk>
2017-02-22 16:12:21 +0000

    adds note for empty page

--- spec/features/achievements_feature_spec.rb
+++ spec/features/achievements_feature_spec.rb
@@ -3,13 +3,18 @@ require 'rails_helper'
 feature "achievements" do
   scenario "user should see link to add achievement" do
     visit "/"
-    expect(page).to have_link "Add Achievement"
+    expect(page).to have_link "Add an achievement"
+  end
+
+  scenario "there are no achievements" do
+    visit "/"
+    expect(page).to have_content "No one has added any achievements yet."
   end
 
   scenario "user should be taken to form to add achievement" do
     visit "/"
-    click_link "Add Achievement"
-    fill_in "Headline", with: "Career fair"
+    click_link "Add an achievement"
+    fill_in "Summary", with: "Career fair"
     fill_in "Description", with: "It was fun"
     fill_in "Categories", with: "Sponsorship, Role model"
     fill_in "Date", with: "2017-02-21"
